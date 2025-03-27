05587a0f1fd000d639cf0bec8f90a7c856696eb6
Aran Fononi <aaronfononi@gmail.com>
2025-03-27 14:46:00 +0330

    Initial Commit

---
+++ TodayToDo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodayToDo
+//
+//  Created by Aran Fononi on 27/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodayToDo/TodayToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayToDoApp.swift
+//  TodayToDo
+//
+//  Created by Aran Fononi on 27/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TodayToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
